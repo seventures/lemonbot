82fc50c69b9bb1391fc92db5a2f5b811bcaf58c2
seven <ssseventure@gmail.com>
2026-06-30 22:55:47 -0500

    bug fixes

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from random import choice
 # initialize
 load_dotenv()
 intents = discord.Intents.all()
-bot = commands.Bot(intents = intents)
+bot = commands.Bot(command_prefix="lemon!", intents = intents)
 
 # commands
 @bot.tree.command(name = "ping", description="Check bot latency")
@@ -18,17 +18,18 @@ async def ping(interaction: discord.Interaction):
 
 @bot.tree.command(name="topic", description="Get a random topic to talk about")
 @app_commands.describe(pings="Users to mention")
-async def topic(interaction: discord.Interaction, pings: discord.Role):
+async def topic(interaction: discord.Interaction, pings: discord.Role = None):
     topics = [
         "*taps mic* yo is this shit working?"
     ]
 
-    if pings.is_default() and not interaction.user.guild_permissions.mention_everyone: # if trying to ping everyone and don't have permissions to ping everyone
-        await interaction.response.send_message("You don't have permission to mention everyone.", ephemeral=True)
-        return
+    if pings is not None:
+        if pings.is_default() and not interaction.user.guild_permissions.mention_everyone: # if trying to ping everyone and don't have permissions to ping everyone
+            await interaction.response.send_message("You don't have permission to mention everyone.", ephemeral=True)
+            return
 
     embed = discord.Embed(title="Topic", description=choice(topics), color=discord.Color.yellow())
-    await interaction.response.send_message(embed=embed, content=pings.mention)
+    await interaction.response.send_message(embed=embed, content=pings.mention if pings else None)
 
 @bot.event
 async def on_ready():
